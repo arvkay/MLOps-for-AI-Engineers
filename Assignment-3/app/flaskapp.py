20ee6572095adf486c66cdae7ee2e9df8a3a89eb
arvkay <ka630207@gmail.com>
2022-01-06 02:43:08 +0530

    Add files via upload

---
+++ Assignment-3/app/flaskapp.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, jsonify
+from assignment_3 import area_of_farmer_field
+
+app = Flask('__main__')
+
+@app.route('/', methods=['GET', 'POST'])
+def get_input():
+    packet = request.get_json(force=True)
+    length = packet['length']
+    width = packet['width']
+    area = area_of_farmer_field(length, width)
+    return {"area":area} 
+
+if __name__ == '__main__':
+    app.run()
